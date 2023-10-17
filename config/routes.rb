f3e0694fd7a2164ec6a24e78db01bebb17f61d39
andykimchris <andrew.muchiri97@gmail.com>
2023-10-17 15:38:10 +0300

    chore: sets devise routes for registration and session controllers

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+  devise_for :users,
+            path_names: { registration: "signup", sign_in: "login", sign_out: "logout"},
+            controllers: { registrations: 'users/registrations', sessions: "users/sessions" }
+
+end
