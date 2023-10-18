bde4563f165b6a27dbf580bacdbbf1bbd44a514f
andykimchris <andrew.muchiri97@gmail.com>
2023-10-18 16:05:18 +0300

    chore: adds private func to validate passwords via regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,20 @@ class User < ApplicationRecord
         :omniauthable,
         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
+
+  validates :email, presence: :true, uniqueness: :true
+  validate :password_matcher
+
+  PASSWORD_REGEX = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}\z/
+
+  private
+
+  def password_matcher
+    unless password.match(PASSWORD_REGEX)
+      errors.add(:password, "must include at least one lowercase letter, one uppercase letter, one digit, and one special character.")
+    end
+  end
+
+
 end
 
