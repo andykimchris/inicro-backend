553a9acb1361e54ab26ada37a7e7e7191b46ea9f
andykimchris <andrew.muchiri97@gmail.com>
2023-10-18 14:35:36 +0300

    chore: adds rspec config & configures color & formatting

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,14 @@ module DeviseVue
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Set rspec as testing framework
+    config.generators do |g|
+      g.test_framework(
+        :rspec,
+        fixtures: false,
+        helper_specs: false,
+      )
+    end
   end
 end
