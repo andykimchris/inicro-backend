097d2f684c86a0fb685f629a44cf9424666aaa0b
andykimchris <andrew.muchiri97@gmail.com>
2023-10-18 16:13:10 +0300

    fix: sets password for factory bot to match regex

--- spec/support/user_helper.rb
+++ spec/support/user_helper.rb
@@ -4,11 +4,11 @@ require "factory_bot_rails"
 module UserHelpers
 
   def create_user
-    FactoryBot.create(:user, email: Faker::Internet.email, password: Faker::Internet.password, is_occupant: false, is_proprietor: true)
+    FactoryBot.create(:user, email: Faker::Internet.email, password: "Johndoe123&", is_occupant: false, is_proprietor: true)
   end
 
   def build_user
-    FactoryBot.build(:user, email: Faker::Internet.email, password: Faker::Internet.password, is_occupant: false, is_proprietor: true)
+    FactoryBot.build(:user, email: Faker::Internet.email, password: "Johndoe123&", is_occupant: false, is_proprietor: true)
   end
 
 end
