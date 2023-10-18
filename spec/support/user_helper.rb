fe7b246815f58865d23c52e4ffaf27dd692ec6e4
andykimchris <andrew.muchiri97@gmail.com>
2023-10-18 14:38:20 +0300

    feat: creates UserHelpers module with test functions

---
+++ spec/support/user_helper.rb
@@ -0,0 +1,15 @@
+require "faker"
+require "factory_bot_rails"
+
+module UserHelpers
+
+  def create_user
+    FactoryBot.create(:user, email: Faker::Internet.email, password: Faker::Internet.password, is_occupant: false, is_proprietor: true)
+  end
+
+  def build_user
+    FactoryBot.build(:user, email: Faker::Internet.email, password: Faker::Internet.password, is_occupant: false, is_proprietor: true)
+  end
+
+end
+
