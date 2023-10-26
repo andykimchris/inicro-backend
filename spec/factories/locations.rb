465771a1e531f196a1e05b58ec667a76a2726804
andykimchris <andrew.muchiri97@gmail.com>
2023-10-26 15:30:23 +0300

    fix: adds a simple factory

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -2,5 +2,11 @@
 
 FactoryBot.define do
   factory :location do
+    longitude { 34.43443 }
+    latitude { 34.435 }
+    name { 'Gendeza Heights' }
+    city { 'Nairobi' }
+    state { 'Nairobi' }
+    country { 'Kenya' }
   end
 end
