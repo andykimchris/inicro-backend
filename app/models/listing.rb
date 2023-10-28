40150d2692f544bf8a3c00ab034903c0df9146a8
andrew kimani <andrew.muchiri97@gmail.com>
2023-10-28 11:05:31 +0300

    006 FEAT: initial seed data (#7)

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,7 +4,10 @@ class Listing < ApplicationRecord
   belongs_to :user, class_name: 'User', primary_key: 'id', inverse_of: :listings
   has_one :location, dependent: :destroy
 
-  enum type: { residential: 0, commercial: 1, land: 2 }
+  enum listing_type: { residential: 0, commercial: 1, land: 2 }
 
   validates :title, :description, presence: true
+
+  HTTPS_URL_REGEX = %r{\Ahttps://\S+\z}
+  validates :site_link, format: { with: HTTPS_URL_REGEX, message: :site_link_format }, allow_blank: true
 end
