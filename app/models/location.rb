569c382f33fb58731ffde67c3afb0479b6d5946e
andykimchris <andrew.muchiri97@gmail.com>
2023-11-13 12:02:06 +0300

    fix: nullifies location_id in listings if destroyed

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Location < ApplicationRecord
-  has_many :listings
+  has_many :listings, dependent: :nullify
 
   validates :name, :description, :address, :city, presence: true
 end
