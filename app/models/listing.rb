c36f6245fbd9f3bf6646218ab77bf9867a7537b0
andykimchris <andrew.muchiri97@gmail.com>
2023-11-13 12:01:36 +0300

    fix: extracts image formats into a constant

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,6 +3,15 @@
 require 'active_storage_validations'
 
 class Listing < ApplicationRecord
+  IMAGE_FORMATS = [
+    'image/png',
+    'image/jpg',
+    'image/jpeg',
+    'image/webp',
+    'image/avif',
+    'image/gif'
+  ].freeze
+
   belongs_to :user, class_name: 'User', primary_key: 'id', inverse_of: :listings
   belongs_to :location
   has_many_attached :images
@@ -10,13 +19,13 @@ class Listing < ApplicationRecord
   enum listing_type: { residential: 0, commercial: 1, land: 2 }
 
   validates :title, :description, presence: true
-  validates :floor_count, presence: true, if: -> { listing_type == 'residential' || listing_type == 'commercial' }
+  validates :floor_count, presence: true, if: -> { listing_type.in?(%w[residential commercial]) }
 
   HTTPS_URL_REGEX = %r{\Ahttps://\S+\z}
   validates :site_link, format: { with: HTTPS_URL_REGEX, message: :site_link_format }, allow_blank: true
 
   validates :images, attached: true,
-                     content_type: ['image/png', 'image/jpg', 'image/jpeg', 'image/webp', 'image/avif', 'image/gif'],
-                     size: { less_than: 5.megabyte },
+                     content_type: IMAGE_FORMATS,
+                     size: { less_than: 3.megabytes },
                      length: { maximum: 15 }
 end
