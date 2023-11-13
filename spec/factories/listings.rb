02e2c5eb9850ae4d87c7a7a300a10c1ec1a11037
andykimchris <andrew.muchiri97@gmail.com>
2023-11-13 12:09:22 +0300

    chore: updates factories for relevant models

--- spec/factories/listings.rb
+++ spec/factories/listings.rb
@@ -2,13 +2,19 @@
 
 FactoryBot.define do
   factory :listing do
-    title { 'MyString' }
-    description { 'MyText' }
-    user { nil }
+    title { 'Listing A' }
+    description { 'Lorem Ipsum' }
     size { 10 }
-    location { nil }
-    metadata { 'MyText' }
+    metadata { 'Extra details about the listing' }
     floor_count { 1 }
     listing_type { 0 }
+    location { association(:location) }
+    user { association(:proprietor_user) }
+
+    after(:create) do |listing|
+      image_path = Rails.root.join('spec/factories/media/images/listing-1.jpg')
+      listing.images.attach(io: Rails.public_path.join(image_path).open, filename: 'listing-1.jpg',
+                            content_type: 'image/jpg')
+    end
   end
 end
