a46724488ac41c66841c23f37361a4f639003e17
andykimchris <andrew.muchiri97@gmail.com>
2023-11-13 17:16:04 +0300

    chore: adds Listing tests for update requests

--- spec/requests/api/v1/listing_spec.rb
+++ spec/requests/api/v1/listing_spec.rb
@@ -35,10 +35,10 @@ RSpec.describe 'Api::V1::Listings' do
     end
 
     context 'when attempting to create a listing as occupant' do
-      let(:occupant_user) { create_occupant_user }
+      let(:proprietor_user) { create_occupant_user }
 
       before do
-        post login_url, params: { user: { email: occupant_user.email, password: occupant_user.password } }
+        post login_url, params: { user: { email: proprietor_user.email, password: proprietor_user.password } }
         image_path = Rails.root.join('spec/factories/media/images/listing-1.jpg')
         listing_params = attributes_for(:listing, location_id: location.id,
                                                   images: [fixture_file_upload(image_path, 'image/jpg')])
@@ -48,6 +48,59 @@ RSpec.describe 'Api::V1::Listings' do
 
       it 'return forbidden (403) status' do
         expect(response).to have_http_status :forbidden
+      end
+    end
+  end
+
+  describe 'PUT /listings' do
+    let(:location) { create_location }
+    let(:update_listing_url) { "/api/v1/listing/#{listing.id}" }
+
+    context 'when user is not authenticated' do
+      let(:proprietor_user) { create_proprietor_user }
+      let(:listing) { create_listing }
+
+      it 'returns unauthorized (401) status' do
+        put update_listing_url, params: { id: listing.id, title: 'New Title' }
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'when user is authenticated' do
+      let(:proprietor_user) { create_proprietor_user }
+      let(:listing) { create_listing }
+      let(:login_url) { '/users/login/' }
+
+      before do
+        post login_url, params: { user: { email: proprietor_user.email, password: proprietor_user.password } }
+      end
+
+      context 'with valid params' do
+        it 'returns ok (200) status' do
+          expect(response).to have_http_status :ok
+        end
+
+        it 'updates the listing' do
+          patch update_listing_url, params: { id: listing.id, title: 'New Title' }
+          listing.reload
+          expect(listing.title).to eq('New Title')
+        end
+      end
+
+      context 'with invalid params' do
+        it 'returns unprocessable entity (422) status' do
+          patch update_listing_url, params: { id: listing.id, title: '' }
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+
+      context 'when the listing is not found' do
+        let(:update_listing_url) { '/api/v1/listing/:id' }
+
+        it 'returns not found status' do
+          put update_listing_url, params: { id: 'invalid_id', title: 'Updated Title' }
+          expect(response).to have_http_status(:not_found)
+        end
       end
     end
   end
