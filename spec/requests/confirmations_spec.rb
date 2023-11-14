c8e841379959340f4257c6c6f32b4256a64c4512
andykimchris <andrew.muchiri97@gmail.com>
2023-11-14 21:13:07 +0300

    frgtrtg

--- spec/requests/confirmations_spec.rb
+++ spec/requests/confirmations_spec.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Confirmations", type: :request do
-  describe "GET /confirmations" do
-    it "works! (now write some real specs)" do
-      expect(100+100).to eq(200)
+RSpec.describe 'Confirmations' do
+  describe 'GET /confirmations' do
+    it 'works! (now write some real specs)' do
+      expect(100 + 100).to eq(200)
     end
   end
 end
