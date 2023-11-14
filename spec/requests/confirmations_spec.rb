001909dd1b3b1e637b3e566c23af7631d7a589c8
andykimchris <andrew.muchiri97@gmail.com>
2023-11-14 21:12:09 +0300

    frgtrtg

---
+++ spec/requests/confirmations_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Confirmations", type: :request do
+  describe "GET /confirmations" do
+    it "works! (now write some real specs)" do
+      expect(100+100).to eq(200)
+    end
+  end
+end
