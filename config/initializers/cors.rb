2007a6fc1155802d2c854d41ed636cc8b23e6e1b
andykimchris <andrew.muchiri97@gmail.com>
2023-11-23 23:31:44 +0300

    chore: set up env variables

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
-    # origins ENV["BASE_URL"]
+    origins ENV["BASE_URL"]
 
     resource "*",
       headers: :any,
