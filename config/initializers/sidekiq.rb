9f114259e90cc29b6c99e91a8456cfb19fb119aa
andykimchris <andrew.muchiri97@gmail.com>
2023-11-27 21:36:23 +0300

    chore: adds sidekiq configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,13 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/1' }
-  config.concurrency = 5
-end
+# require 'sidekiq'
+# require 'sidekiq/web'
+# require 'sidekiq/api'
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/1' }
-  config.concurrency = 5
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: 'redis://localhost:6379/1' }
+#   config.concurrency = 5
+# end
+
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: 'redis://localhost:6379/1' }
+#   config.concurrency = 5
+# end
