afe27812f78588953c60b3760d3648e753dd1ed9
andykimchris <andrew.muchiri97@gmail.com>
2023-11-27 20:33:54 +0300

    chore: sets sidekiq as queue adapter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,8 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "devise_vue_production"
+  config.active_job.queue_adapter     = :sidekiq
+  config.active_job.queue_name_prefix = "inicro_production"
 
   config.action_mailer.default_url_options = { host: Rails.application.credentials.base_url
 }
