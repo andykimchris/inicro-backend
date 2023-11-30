7491bb56273f89aac832625a210ad681ad8f6a55
andykimchris <andrew.muchiri97@gmail.com>
2023-11-30 23:37:21 +0300

    chore: adds config for sidekiq-cron

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   require 'sidekiq/web'
+  require 'sidekiq/cron/web'
+
   Rails.application.routes.draw do
     mount Sidekiq::Web => '/sidekiq'
   end
