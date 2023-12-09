f7d8da2260a20cf6489085fa7ab411e80f26f66a
andrew kimani <andrew.muchiri97@gmail.com>
2023-12-09 11:50:08 +0300

    FEAT 011: Sidekiq Integration (#14)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,10 +60,6 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "devise_vue_production"
-
   config.action_mailer.default_url_options = { host: Rails.application.credentials.base_url
 }
   config.action_mailer.perform_caching = false
---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,10 @@
+require 'sidekiq'
+require 'sidekiq/web'
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: Rails.application.credentials.redis_url }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: Rails.application.credentials.redis_url }
+end
