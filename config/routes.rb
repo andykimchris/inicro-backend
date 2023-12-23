3853e5e1d23e7fff276f558f0ff52a06b9c5feed
andykimchris <andrew.muchiri97@gmail.com>
2023-12-23 11:40:41 +0300

    chore: renames listing to listings

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :listing, only: %i[create show update]
+      resources :listings, only: %i[create show update]
     end
   end
 end
