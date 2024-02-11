7601c0bf27fe393bb2a977df0e18a3c2c5f86f43
andykimchris <andrew.muchiri97@gmail.com>
2024-02-11 20:59:04 +0300

    chore: generates qr code

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -8,6 +8,7 @@ class Unit < ApplicationRecord
   has_many :users, through: :unit_wishlists
 
   has_one_attached :floorplan_image
+  has_one_attached :qrcode, dependent: :destroy
   has_many_attached :images
 
   enum unit_type: { studio: 0, '1-bedroom': 1, '2-bedroom': 2, '3-bedroom': 3 }
@@ -25,11 +26,38 @@ class Unit < ApplicationRecord
 
   validate :user_is_occupant
 
+  before_commit :generate_qrcode, on: :create
+
   private
 
   def user_is_occupant
     return unless user&.is_proprietor?
 
     errors.add(:user, 'must be an occupant')
+  end
+
+  def generate_qrcode
+    host = Rails.application.config.action_controller.default_url_options[:host]
+    assign_unit_to_user_path = "http://#{host}/unit/#{id}"
+    qr = RQRCode::QRCode.new(assign_unit_to_user_path)
+
+    file = qr.as_png(
+      bit_depth: 1,
+      border_modules: 4,
+      color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+      color: 'black',
+      file: nil,
+      fill: 'white',
+      module_px_size: 6,
+      resize_exactly_to: false,
+      resize_gte_to: false,
+      size: 120
+    )
+
+    qrcode.attach(
+      io: StringIO.new(file.to_s),
+      filename: 'qrcode.png', # TODO: Rename filename
+      content_type: 'image/png'
+    )
   end
 end
