94d9ab554ec8e03b933c9975b67ee55431a72c08
andykimchris <andrew.kimani@renofi.com>
2024-02-18 18:05:18 +0300

    create Booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_07_155938) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_18_145933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -41,6 +41,19 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_07_155938) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "unit_id", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.string "email"
+    t.datetime "booking_set_at"
+    t.string "booking_assigned_to_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["unit_id"], name: "index_bookings_on_unit_id"
   end
 
   create_table "jwt_denylist", force: :cascade do |t|
@@ -145,6 +158,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_07_155938) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "units"
   add_foreign_key "listing_amenities", "listings"
   add_foreign_key "listings", "locations"
   add_foreign_key "listings", "users"
