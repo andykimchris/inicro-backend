3b375ef6dc0378ee85cdcc0811a0c4dc8803ee3a
andykimchris <andrew.muchiri97@gmail.com>
2025-01-09 15:35:53 +0300

    add sidekiq setup to credentials

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,9 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+#
+require 'sidekiq/testing'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -45,6 +48,10 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.before do
+    Sidekiq::Worker.clear_all
+  end
 
   # The settings below are suggested to provide a good initial experience
   # with RSpec, but feel free to customize to your heart's content.
@@ -91,4 +98,5 @@ RSpec.configure do |config|
   #   # test failures related to randomization by passing the same `--seed` value
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
+  #
 end
